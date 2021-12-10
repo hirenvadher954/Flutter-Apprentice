1d66a21dc14e7b642bdfea6b32b6fc194ca5c381
hiren <hirenvadher954@gmail.com>
2021-12-10 23:48:31 +0530

    theme support added

--- fooderlich/lib/main.dart
+++ fooderlich/lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 void main() {
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
   const Fooderlich({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
     return MaterialApp(
+      theme: theme,
       title: 'Fooderlich',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Fooderlich')),
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+        appBar: AppBar(
+            title: Text(
+          'Fooderlich',
+          style: theme.textTheme.headline6,
+        )),
+        body: const Center(
+            child: Text(
+          'Let\'s get cooking 👩‍🍳',
+        )),
       ),
     );
   }
