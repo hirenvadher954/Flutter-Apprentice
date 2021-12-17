25d597cc65e78e0cff1856129112cf54c9d073e8
hiren <hirenvadher954@gmail.com>
2021-12-17 22:42:43 +0530

    Removed unncessary comment

--- fooderlich/lib/screens/empty_grocery_screen.dart
+++ fooderlich/lib/screens/empty_grocery_screen.dart
@@ -10,12 +10,9 @@ class EmptyGroceryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 1
     return Padding(
       padding: const EdgeInsets.all(30.0),
-      // 2
       child: Center(
-        // 3
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
