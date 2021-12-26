4a9e7d897633a25c8d3b2580845de291cfde186a
hiren <hirenvadher954@gmail.com>
2021-12-26 15:29:18 +0530

    AppRouter Modified

--- fooderlich_navigation/lib/main.dart
+++ fooderlich_navigation/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'screens/splash_screen.dart';
+import 'navigation/app_router.dart';
 // TODO: Import app_router
 
 void main() {
@@ -20,12 +20,21 @@ class Fooderlich extends StatefulWidget {
 }
 
 class _FooderlichState extends State<Fooderlich> {
+  final _appStateManager = AppStateManager();
   final _groceryManager = GroceryManager();
   final _profileManager = ProfileManager();
-  // TODO: Create AppStateManager
-  // TODO: Define AppRouter
 
-  // TODO: Initialize app router
+  late AppRouter _appRouter;
+
+  @override
+  void initState() {
+    super.initState();
+    _appRouter = AppRouter(
+      appStateManager: _appStateManager,
+      groceryManager: _groceryManager,
+      profileManager: _profileManager,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +46,9 @@ class _FooderlichState extends State<Fooderlich> {
         ChangeNotifierProvider(
           create: (context) => _profileManager,
         ),
-        // TODO: Add AppStateManager ChangeNotifierProvider
+        ChangeNotifierProvider(
+          create: (context) => _appStateManager,
+        )
       ],
       child: Consumer<ProfileManager>(
         builder: (context, profileManager, child) {
@@ -52,7 +63,10 @@ class _FooderlichState extends State<Fooderlich> {
             theme: theme,
             title: 'Fooderlich',
             // TODO: Replace with Router widget
-            home: const SplashScreen(),
+            home: Router(
+              routerDelegate: _appRouter,
+              // TODO: Add backButtonDispatcher
+            ),
           );
         },
       ),
---
+++ fooderlich_navigation/lib/navigation/app_router.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+// 1
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  final AppStateManager appStateManager;
+
+  final GroceryManager groceryManager;
+
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+  // 6
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: _handlePopPage,
+      pages: [
+        if (!appStateManager.isInitialized) SplashScreen.page(),
+
+        if (appStateManager.isInitialized && !appStateManager.isLoggedIn)
+          LoginScreen.page(),
+        if (appStateManager.isLoggedIn && !appStateManager.isOnboardingComplete)
+          OnboardingScreen.page(),
+
+        if (appStateManager.isOnboardingComplete)
+          Home.page(appStateManager.getSelectedTab),
+
+        if (groceryManager.isCreatingNewItem)
+          GroceryItemScreen.page(
+            onCreate: (item) {
+              groceryManager.addItem(item);
+            },
+            onUpdate: (item, index) {},
+          ),
+        // TODO: Select GroceryItemScreen
+        // TODO: Add Profile Screen
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+    if (route.settings.name == FooderlichPages.onboardingPath) {
+      appStateManager.logout();
+    }
+    // TODO: Handle state when user closes grocery item screen
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+    return true;
+  }
+
+  // 10
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
